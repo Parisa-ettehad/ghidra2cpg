2568b496b13ec6fa48695bf1ee5db1900a24bc9b
Parisa Ettehad <parisa98ettehad@gmail.com>
2026-04-22 00:04:16 -0400

    message

---
+++ uaf_real.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+void bad() {
+    int *p = malloc(sizeof(int));
+    free(p);
+    *p = 10; // ❌ use-after-free
+}
+
+void good() {
+    int *p = malloc(sizeof(int));
+    *p = 20;
+    free(p); // ✅ safe
+}
+
+int main() {
+    bad();
+    good();
+    return 0;
+}
